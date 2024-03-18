1886b6844a453f16b9dee905c699f4d52a0c3db9
WhoStoleMyCoffee <etiennek.paradisf@gmail.com>
2024-03-18 11:06:29 +0900

    Playground example

--- addons/optional/examples/misc/misc.gd
+++ addons/optional/examples/misc/misc.gd
@@ -8,44 +8,13 @@ Note:
 	Here, they're not because we can't define statics inside functions
 """
 
-""" EXPERIMENTAL
-This example demonstrates the usage of Enums and EnumVars
-"""
-func example_process():
-	var ProcessStates: Enum = Enum.new()\
-		.add(&"Processing", { 'percentage': 0.0 })\
-		.add(&"Done") # We don't need to store percentage when done!
-	
-	var state: EnumVar = ProcessStates.Processing
-	
-	while true:
-		print_console("state = %s" % state)
-		
-		match state.variant:
-			&"Processing":
-				state.percentage += 0.1
-				if state.percentage >= 1.0:
-					state = ProcessStates.Done
-			&"Done":
-				print_console("Done!")
-				break
 
-""" EXPERIMENTAL
-This example demonstrates the usage of EnumDicts
-Notice how pet_state is a Dictionary
 """
-func example_pet_state():
-	var PetState: EnumDict = EnumDict.new()\
-		.add(&"Sleeping")\
-		.add(&"Following", { "player": "owner" })\
-		.add(&"Idle")
-	
-	var pet_state: Dictionary = PetState.Idle
-	print_console("pet_state = %s" % pet_state) # Kind of ugly
-	print_console("pet_state = %s" % EnumDict.stringify(pet_state)) # Prettier, but more verbose in code
-	
-	pet_state = PetState.variant(&"Following", { "player": "player2" })
-	print_console("pet_state = %s" % EnumDict.stringify(pet_state))
+PLAYGROUND FOR TESTING
+"""
+func playground():
+	print_console("See the function `func playground()`")
+	print_console("Use `print_console` to print to this console!")
 
 
 """
@@ -88,9 +57,57 @@ func example_combo():
 
 
 
+#region EXPERIMENTAL
+
+"""
+This example demonstrates the usage of Enums and EnumVars
+"""
+func example_process():
+	var ProcessStates: Enum = Enum.new()\
+		.add(&"Processing", { 'percentage': 0.0 })\
+		.add(&"Done") # We don't need to store percentage when done!
+	
+	var state: EnumVar = ProcessStates.Processing
+	
+	while true:
+		print_console("state = %s" % state)
+		
+		match state.variant:
+			&"Processing":
+				state.percentage += 0.1
+				if state.percentage >= 1.0:
+					state = ProcessStates.Done
+			&"Done":
+				print_console("Done!")
+				break
+
+""" EXPERIMENTAL
+This example demonstrates the usage of EnumDicts
+Notice how pet_state is a Dictionary
+"""
+func example_pet_state():
+	var PetState: EnumDict = EnumDict.new()\
+		.add(&"Sleeping")\
+		.add(&"Following", { "player": "owner" })\
+		.add(&"Idle")
+	
+	var pet_state: Dictionary = PetState.Idle
+	print_console("pet_state = %s" % pet_state) # Kind of ugly
+	print_console("pet_state = %s" % EnumDict.stringify(pet_state)) # Prettier, but more verbose in code
+	
+	pet_state = PetState.variant(&"Following", { "player": "player2" })
+	print_console("pet_state = %s" % EnumDict.stringify(pet_state))
+
+#endregion
+
+
+#region META
+
 func print_console(string: String):
 	$VSplitContainer/RichTextLabel.text += '\n' + string
 
 func _on_button_pressed(method: String):
 	print_console('\nRunning demo "%s"' % method)
 	call(method)
+
+#endregion
